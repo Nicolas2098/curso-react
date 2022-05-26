ced48c278737f8dd08afea109d4d944b3a683eb9
Nicolas2098 <nicolasmborges@gmail.com>
2022-05-26 10:19:33 -0300

    correcion webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,59 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
-    // ... Configuración de empaquetado
     entry: './src/index.js',
+    
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+        publicPath: '/'
     },
     mode: 'development',
     resolve: {
-        extensions:['.js','.jsx']
+        extensions: ['.js','.jsx']
     },
+
     module: {
-        // ... Lista de reglas respecto a los loaders	
-        rules : [
-            // Reglas para babel
+        rules: [
             {
-                test: /\.(js | jsx)$/,
-                use: { loader: 'babel-loader'},
-                exclude: /node_modules/
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
             },
-            // Reglas para HTML loader
             {
-		test: /\.html$/,
-		use: [{ loader: 'html-loader'}]
-           },
-           {
-            test: /\.s[ac]ss$/i,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
-        }
-
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            },
+            {
+              test: /\.(css||scss)$/,
+              use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            }
         ]
-
     },
-    plugins: [
-	    //... Configuración de plugins
-        new HtmlWebpackPlugin(
-		{ 
-      		template: './public/index.html', 
-		    filename: './index.html'   
-		}
-	),
-    new MiniCssExtractPlugin({
-        filename: '[name].css'
-    })
-	]
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    devServer: {
+        historyApiFallback: true,
+    }
+    
 }
