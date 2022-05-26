9b4b3160fb7966f371162e2af631c9b5836571bb
Nicolas2098 <nicolasmborges@gmail.com>
2022-05-26 01:20:14 -0300

    mm

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import ReactDom from "react-dom";
+import { createRoot } from 'react-dom/client';
+import App from "./routes/App";
+
+const container = document.getElementById('app');
+const root = createRoot(container); 
+root.render(<App tab="home" />);
+
+//ReactDom.render(<App />, document.getElementById('app'));
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/NotFound.scss'
+
+const NotFound = () => {
+    return (
+        <div className='notFound'>
+                <div className="notFound-404">
+                    <span id='span404'>404</span>
+                </div>
+                <div className='notFound-Msg'>
+                    <span id='spanMsg'>Pagina no encontrada... <b>:(</b></span>
+                </div>
+            </div>
+    );
+};
+
+export default NotFound;
